260b92f80213405d3c49aadc3cb8a5767d9c490a
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2022-11-12 21:46:51 +0530

    adds a logger

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
-
+const router = require('./routes/index');
+let logger = require('morgan');
 const app = express()
+
+// Add a routers
+
 
 app.get('/',(req,res)=>{
     res.send("<h2>Hello There adsufh  !!</h2>")
 })
+
+app.use(logger(`:remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :: (time: :response-time ms)`));
 
 
 const port = process.env.PORT || 3000
