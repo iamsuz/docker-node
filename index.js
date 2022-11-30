5110f6112cf0d1b755a763357558f387c6fcdeea
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2022-11-30 18:14:40 +0530

    adds template engine

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express')
 const router = require('./routes/index');
 let logger = require('morgan');
 const app = express()
-
+const bodyParser = require('body-parser')
+const session = require('cookie-session')
 // Add a routers
 
-
+app.use(session({secret: process.env.SECRET}))
+    .use(function(req,res,next){
+        next()
+    })
+    .get('/todo', function(req,res){
+        res.render('todo.ejs',{todolist: req.session.todolist})
+    })
+    .post('/todo/add',function(req,res){
+        if(req.body.newtodo != ''){
+            req.session.todolist.push(req.body.newtodo)
+        }
+        res.redirect('/todo')
+    })
+    .use(function(req,res,next){
+        res.redirect('/todo')
+    })
+    
 app.get('/',(req,res)=>{
     res.send("<h2>Hello There adsufh  !!</h2>")
 })
